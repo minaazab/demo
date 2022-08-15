db229422d79f095e022ebed53a66888b190f874e
SamuelASC22 <109676951+SamuelASC22@users.noreply.github.com>
2022-08-15 10:55:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//Grade calc 
+
+const grade = () => {
+    const Math = document.getElementById('Math').value;
+    const MathNum = parseFloat(Math);
+    const Science = document.getElementById('Science').value;
+    const ScienceNum = parseFloat(Science);
+    const History = document.getElementById('History').value;
+    const HistoryNum = parseFloat(History);
+    const English = document.getElementById('English').value;
+    const EnglishNum = parseFloat(English);
+    const Language = document.getElementById('Language').value;
+    const LanguageNum = parseFloat(Language);
+    const Physical_Education = document.getElementById('Physical Education').value;
+    const Physical_EducationNum = parseFloat(Physical_Education);
+    const Additional_Class = document.getElementById('Additional Class').value;
+    const Additional_ClassNum = parseFloat(Additional_Class);
+    const Additional_Class2 = document.getElementById('Additional Class').value;
+    const Additional_Class2Num = parseFloat(Additional_Class2);
+    
+}
+
