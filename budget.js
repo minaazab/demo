bd44b3335658b645c97c72b164966927dc7b58b2
SamuelASC22 <109676951+SamuelASC22@users.noreply.github.com>
2022-08-16 14:14:24 -0400

    Update budget.js

--- budget.js
+++ budget.js
@@ -1,20 +1,23 @@
 // elements
 let budget = document.getElementById('budget').value;
-let entertainment = document.getElementById('entertainment').value;
-let savings = document.getElementById('savings').value;
-let food = document.getElementById('food').value;
-let entertainmentoutput = document.getElementById("entertainmentoutput");
-let savingsoutput = document.getElementById("savingsoutput");
-let foodoutput = document.getElementById("foodoutput");
+let entertainmentoutput = document.getElementById('entertainmentoutput');
+let savingsoutput = document.getElementById('savingsoutput');
+let foodoutput = document.getElementById('foodoutput');
+let button = document.getElementById('budgetme');
+button.addEventListener("click", budgeter());
 
 // mathematics
-let entertainmentbudget = budget*0.20;
-let savingsbudget = budget*0.60;
-let foodbudget = budget*0.20;
-
+function budgeter(event) {
+    event.preventDefault();
+    let entertainmentbudget = budget * 0.20;
+    let savingsbudget = budget * 0.60;
+    let foodbudget = budget * 0.20;
+    entertainmentoutput.innterHTML = 'You should spend $ ${entertainmentbudget}, on your form of entertainment!';
+    savingsoutput.innerHTML = "You should save " + "$" + entertainmentbudget + "!";
+    foodoutput.innerHTML = "You should spend " + "$" + foodbudget + " on your food!";
+}
 
 // printing
-
-entertainmentoutput.innterHTML = "You should spend " + "$" + entertainmentbudget + " on your form of entertainment!";
-savingsoutput.innerHTML = "You should save " + "$" + entertainmentbudget + "!";
-foodoutput.innerHTML = "You should spend " + "$" + foodbudget + " on your food!";
+if (budget.length == ""){
+    alert("Please enter a budget!");
+}
