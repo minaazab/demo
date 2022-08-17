d031aee2755bae4d3fc0d402582189a6198c1d6b
SamuelASC22 <109676951+SamuelASC22@users.noreply.github.com>
2022-08-17 18:38:32 -0400

    Update grade.js

--- grade.js
+++ grade.js
@@ -1,54 +1,48 @@
-//Grade calc 
-
-const grade = () => {
-    const Math = document.getElementById('Algebra');
-    const MathNum = parseFloat(Math.value);
-    const Science = document.getElementById('Physics');
-    const ScienceNum = parseFloat(Science.value);
-    const History = document.getElementById('Global History');
-    const HistoryNum = parseFloat(History.value);
-    const English = document.getElementById('English').value;
-    const EnglishNum = parseFloat(English);
-    const Physical_Education = document.getElementById('Physical Education').value;
-    const Physical_EducationNum = parseFloat(Physical_Education);
-    const Additional_Class = document.getElementById('Music').value;
-    const Additional_ClassNum = parseFloat(Additional_Class);
-    const Additional_Class2 = document.getElementById('Statistics').value;
-    const Additional_Class2Num = parseFloat(Additional_Class2);
-    
-
-    let total = MathNum + ScienceNum + HistoryNum + EnglishNum + Physical_EducationNum + Additional_ClassNum + Additional_Class2Num;
-
-if ( Math == 0 || Science == 0 || History == 0 || English == 0 || Physical_Education == 0 || Additional_Class == 0 || Additional_Class2 == 0 ) {
-        alert('Enter Grade');
+const button = document.getElementById('button')
+ 
+ 
+function calc() {
+let math = document.getElementById('math').value
+let science = document.getElementById('science').value
+let history = document.getElementById('history').value
+let english = document.getElementById('english').value
+let pe = document.getElementById('pe').value
+let add = document.getElementById('add').value
+let add_2 = document.getElementById('add_2').value
+ 
+let total = parseFloat(math) + parseFloat(science) + parseFloat(history) + parseFloat(english) + parseFloat(pe) + parseFloat(add) + parseFloat(add_2)
+ 
+let percentage = (total/7)
+ 
+let grades = ""
+ 
+if(percentage >= 95  && percentage >= 100){
+  grades = 'A+';
+}else if(percentage >= 90  && percentage <= 94){
+   grades = 'A';
+}else if(percentage >= 80  && percentage <= 89){
+   grades = 'B';
+}else if(percentage >= 70  && percentage <= 79){
+  grades = 'C';
+}else if(percentage >= 65  && percentage <= 69){
+  grades = 'D';
+}else if(percentage >= 0  && percentage <= 64){
+  grades = 'F';
 }
-
-
-let percentage = (total/700) * 100;    
-
-  if(percentage >= 95  && percentage >= 100){
-    grades = 'A+';
-  }else if(percentage >= 90  && percentage <= 94){
-     grades = 'A';
-  }else if(percentage >= 80  && percentage <= 89){
-     grades = 'B';
-  }else if(percentage >= 70  && percentage <= 79){
-    grades = 'C';
- }else if(percentage >= 65  && percentage <= 69){
-    grades = 'D';
- }else if(percentage >= 0  && percentage <= 64){
-    grades = 'F';
+ 
+var percentelement = document.getElementById('Result')
+ 
+ 
+ 
+ 
+//percentage.innerHTML = "You have a " + percentage + "% average.";
+//grades.innerHTML = "Your letter grade is " + grades + ".";
+ 
+var percentplacementelement = document.getElementById('per')
+percentplacementelement.innerHTML = "You have a " + percentage + "% average.";
+ 
+ 
+var gradeelement = document.getElementById('gra')
+gradeelement.innerHTML = "Your letter grade is " + grades + ".";
 }
-
-
-
-function showresult(){
-    document.getElementById('getTotal').innerText = 'Out of 700 your total is ' + total;
-    document.getElementById('getPercentage').innerText = 'percentage is' + ' ' + percentage + ' %';
-    document.getElementById('getGrade').innerText = 'Your grade is' + ' ' + grades;
-  }
-  } 
-  
-
-
-
+button.addEventListener("click", calc);
